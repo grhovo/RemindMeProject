d489b99e4cb11b890eca91a04584ded23aa9f73b
Hovo <37000448+grhovo@users.noreply.github.com>
2018-10-11 15:50:08 +0400

    Added ToolBar and icon for search

--- app/src/main/java/com/android/hovo/remindme/MainActivity.java
+++ app/src/main/java/com/android/hovo/remindme/MainActivity.java
@@ -3,11 +3,22 @@ package com.android.hovo.remindme;
 import android.os.Bundle;
 import android.support.annotation.Nullable;
 import android.support.v7.app.AppCompatActivity;
+import android.support.v7.widget.Toolbar;
+import android.view.Menu;
+import android.view.MenuItem;
 
 public class MainActivity extends AppCompatActivity {
+
+
     @Override
     protected void onCreate(@Nullable Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+    }
+
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu) {
+        getMenuInflater().inflate(R.menu.menu_toolbar,menu);
+        return true;
     }
 }
