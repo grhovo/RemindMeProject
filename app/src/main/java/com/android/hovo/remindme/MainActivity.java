ae537bcedcf6bb2249bd48723df16f452b952b23
Hovo <37000448+grhovo@users.noreply.github.com>
2018-10-12 17:36:15 +0400

    Added Navigation

--- app/src/main/java/com/android/hovo/remindme/MainActivity.java
+++ app/src/main/java/com/android/hovo/remindme/MainActivity.java
@@ -8,17 +8,26 @@ import android.view.Menu;
 import android.view.MenuItem;
 
 public class MainActivity extends AppCompatActivity {
+    private Toolbar mToolbar;
 
 
     @Override
     protected void onCreate(@Nullable Bundle savedInstanceState) {
+        setTheme(R.style.AppDefault);
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        initToolbar();
     }
 
-    @Override
-    public boolean onCreateOptionsMenu(Menu menu) {
-        getMenuInflater().inflate(R.menu.menu_toolbar,menu);
-        return true;
+    private void initToolbar() {
+        mToolbar = findViewById(R.id.toolbar);
+        mToolbar.setTitle(R.string.app_name);
+        mToolbar.setOnMenuItemClickListener(new Toolbar.OnMenuItemClickListener() {
+            @Override
+            public boolean onMenuItemClick(MenuItem menuItem) {
+                return false;
+            }
+        });
+        mToolbar.inflateMenu(R.menu.menu_toolbar);
     }
 }
